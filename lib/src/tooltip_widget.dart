86530b13e5f6443206656bf027f38aadb17d9aba
leary <leary1@yandex.ru>
2024-11-28 00:38:55 +0600

    feat: add tooltip & tooltip button

--- lib/src/tooltip_widget.dart
+++ lib/src/tooltip_widget.dart
@@ -1,36 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:spotlight_ui/src/tooltip_button.dart';
 
 class TooltipWidget extends StatelessWidget {
   final Function() onNextStep;
   final Function() onSkip;
-  const TooltipWidget(
-      {super.key, required this.onNextStep, required this.onSkip});
+  final TooltipButton? nextStepButton;
+  final TooltipButton? skipButton;
+  final double? width;
+  final double? height;
+  final double? spacing;
+  final Color? backgroundColor;
+  final BorderRadiusGeometry? borderRadius;
+  final Widget? child;
+
+  const TooltipWidget({
+    super.key,
+    required this.onNextStep,
+    required this.onSkip,
+    this.nextStepButton,
+    this.skipButton,
+    this.width,
+    this.height,
+    this.spacing,
+    this.backgroundColor,
+    this.borderRadius,
+    this.child,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(22),
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(16),
+    return SizedBox(
+      width: width ?? MediaQuery.of(context).size.width,
+      height: height,
+      child: DecoratedBox(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(22),
+        ),
         child: Column(
           children: [
-            Text(
-              "Смените жилой квартал, если хотите потренироваться в другом месте",
-              style: Theme.of(context).textTheme.bodyMedium,
+            child ?? const SizedBox.shrink(),
+            SizedBox(height: spacing ?? 0),
+            InkWell(
+              onTap: onNextStep,
+              child: nextStepButton ?? const SizedBox.shrink(),
             ),
-            const SizedBox(height: 12),
-            ElevatedButton(onPressed: onNextStep, child: Text("data")),
-            const SizedBox(height: 8),
-            GestureDetector(
-                onTap: onSkip,
-                child: Text(
-                  "asdasd",
-                  style: Theme.of(context).textTheme.bodyMedium,
-                )),
+            SizedBox(height: spacing ?? 0),
+            InkWell(
+              onTap: onSkip,
+              child: nextStepButton ?? const SizedBox.shrink(),
+            ),
           ],
         ),
       ),
